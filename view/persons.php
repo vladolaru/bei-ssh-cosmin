60448ea0ebad473c360d65296e888a64dd3e025c
BurloiuCosmin <cosmin.burloiu@yahoo.com>
2018-08-13 11:45:14 +0300

    finished almost all forms, one more to go

---
+++ view/persons.php
@@ -0,0 +1,55 @@
+<?php include_once 'header.php'; ?>
+
+<html>
+<body>
+
+
+<section class="section">
+	<div class="container">
+		<div class="notification">
+
+			<p class="has-text-black has-text-centered"><strong>Your Secret Santa players</strong></p>
+			<br>
+
+			<nav class="level">
+				<p class="level-item has-text-centered">
+					<strong>1.First Last name</strong>
+				</p>
+				<p class="level-item has-text-centered">
+					<strong>test@test.com</strong>
+				</p>
+				<p class="level-item has-text-centered">
+					<span class="icon has-text-danger">
+                        <i class="fas fa-ban"></i>
+					</span>
+				</p>
+
+			</nav>
+
+				<div class="field is-grouped">
+					<div class="control">
+						<br>
+						<a href="http://pixy.local/ssh/view/addedit.php" class="button is-danger is-rounded">Add a new one</a>
+					</div>
+				</div>
+			</div>
+
+			<div class="level">
+				<div class="container">
+					<div class="container has-text-center">
+						<a class="button is-rounded is-black" href="http://pixy.local/ssh/view/roundhistory.php">Back to</a>
+					</div>
+
+				</div>
+			</div>
+
+
+		</div>
+	</div>
+</section>
+
+
+</body>
+</html>
+
+<?php include_once 'footer.php'; ?>
---
+++ view/rounds.php
@@ -0,0 +1,76 @@
+<?php include_once 'header.php'; ?>
+
+<html>
+<body>
+
+
+<section class="section">
+	<div class="container">
+		<div class="notification">
+
+			<p class="has-text-black has-text-centered"><strong>Let's get this going..</strong></p>
+			<br>
+
+			<div class="field">
+				<br>
+				<label class="label">Choose your participants<ion-icon name="text"></ion-icon> </label>
+				<div class="field">
+					<div class="control">
+						<textarea class="textarea is-primary" type="text" placeholder="Persons that will participate in the event"></textarea>
+					</div>
+				</div>
+
+				<label class="label">Recommended budget<ion-icon name="cash"></ion-icon> </label>
+				<div class="control ">
+					<input class="input is-primary" type="text" placeholder="The maximum sum" value="">
+				</div>
+				<br>
+
+				<label class="label">Email title(template) <ion-icon name="mail"></ion-icon> </label>
+				<div class="control ">
+					<input class="input is-primary" type="text" placeholder="Email title" value="">
+				</div>
+				<br>
+
+				<label class="label">Email from <ion-icon name="mail"></ion-icon> </label>
+				<div class="control ">
+					<input class="input is-primary" type="email" placeholder="Email address" value="">
+				</div>
+				<br>
+
+				<label class="label">Email content(template)<ion-icon name="text"></ion-icon> </label>
+				<div class="field">
+					<div class="control">
+						<textarea class="textarea is-primary" type="text" placeholder="The message that everyone will receive"></textarea>
+					</div>
+				</div>
+
+				<div class="field is-grouped">
+					<div class="control">
+						<br>
+						<button class="button is-danger is-rounded">Send emails</button>
+					</div>
+				</div>
+			</div>
+
+			<div class="level">
+				<div class="container">
+					<p>or...</p>
+					<br>
+					<div class="container has-text-center">
+						<a class="button is-rounded is-black" href="http://pixy.local/ssh/view/roundhistory.php">Cancel</a>
+					</div>
+
+				</div>
+			</div>
+
+
+		</div>
+	</div>
+</section>
+
+
+</body>
+</html>
+
+<?php include_once 'footer.php'; ?>
